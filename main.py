7e11215dbcb83927f64b7f8273df4902c191d867
Cornel Grimm <cornel.grimm@gmx.ch>
2023-11-09 18:49:36 +0100

    importing password

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+from secret import password
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     print_hi('Cornel')
 
     print("How are you doing?")
-
     print("Where do you live?")
+#    print(password)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
